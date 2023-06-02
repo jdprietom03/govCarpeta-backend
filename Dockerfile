d60b40f716942c964b71ab5ec74539b2f337f24f
jdprietom03 <jdprietom@eafit.edu.co>
2023-06-02 00:47:26 -0500

    Configuraciones de despliegue

--- Dockerfile
+++ Dockerfile
@@ -7,4 +7,5 @@ FROM openjdk:17
 WORKDIR /backend
 EXPOSE 80
 COPY --from=build /backend/build/libs/backend-0.0.1-SNAPSHOT.jar .
+COPY static/key.json /backend/static/key.json
 CMD java -jar backend-0.0.1-SNAPSHOT.jar
--- src/main/java/com/eafit/backend/BackendApplication.java
+++ src/main/java/com/eafit/backend/BackendApplication.java
@@ -6,28 +6,11 @@ import java.io.IOException;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Import;
-import com.google.auth.oauth2.GoogleCredentials;
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.FirebaseOptions;
 
 @SpringBootApplication
 public class BackendApplication {
 
 	public static void main(String[] args) throws IOException {
 		SpringApplication.run(BackendApplication.class, args);
-
-		String pathToServiceAccountJson = BackendApplication.class.getClassLoader().getResource("static/key.json").getPath();
-
-        // Carga las credenciales desde el archivo JSON
-        FileInputStream serviceAccount = new FileInputStream(pathToServiceAccountJson);
-
-        // Configura Firebase Admin SDK
-        FirebaseOptions options = new FirebaseOptions.Builder()
-                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-                .build();
-
-        FirebaseApp.initializeApp(options);
 	}
-
 }
